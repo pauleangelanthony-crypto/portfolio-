a078f36a19496701610081fc7619d595879ba9e9
jupiter221208 <jupiter221208@gmail.com>
2025-10-16 07:29:38 -0400

    add space

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -23,7 +23,7 @@ export async function POST(request: Request) {
       to,
       subject: subject || `Message from ${name || "website"}`,
       text: message,
-      html: `<div>User Name:<strong>${name || ""}</strong></div><div>Email: ${
+      html: `<div>User Name: <strong>${name || ""}</strong></div><div>Email: ${
         from || ""
       }</div><div>Message: ${message || ""}</div>`,
     };
